1be4e8fba36f5a4ec229b8472bb519e00ffe9fdc
Tyler Gannon <tgannon@gmail.com>
2023-02-22 16:47:27 -0600

    Add build for dev server.

--- servers/node/docker.fish
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env fish
-
-activate_docker (realpath (dirname (status -f))) tylergannon/node-alpine
--- servers/unit/docker.fish
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env fish
-
-activate_docker (realpath (dirname (status -f))) tylergannon/nginx-unit-alpine
-
