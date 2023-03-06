84ffc7656b8f58ebec03991a7c1042413cb59d1a
Tyler Gannon <tgannon@gmail.com>
2023-03-06 01:55:32 +0000

    Reorganize navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,6 @@
 
 <svelte:body />
 <div class="flex h-screen flex-col bg-base-100 dark:bg-black-900">
-  <div class="navbar bg-base-200">
-    <a href="#" class="btn btn-ghost normal-case text-xl">TransitHD</a>
-    <div class="navbar-center">Hello, good sky!</div>
-  </div>
 
 	<main class="flex-1">
 		<slot />
