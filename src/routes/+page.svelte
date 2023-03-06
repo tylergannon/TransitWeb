c258ca121ec35e1e1c4410d757fc33c358de83ce
Tyler Gannon <tgannon@gmail.com>
2023-03-06 01:55:32 +0000

    Reorganize navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,16 +6,22 @@
 </svelte:head>
 
 
-<div class="h-full v-full bg-gray-300">
-	<div class="hero min-h-screen">
-		<div class="hero-overlay bg-opacity-60"></div>
-		<div class="hero-content text-center text-neutral-content">
-			<div class="max-w-md">
-				<h1 class="mb-5 text-5xl font-bold">Transit HD</h1>
-				<p class="mb-5">It will be pretty expensive but you're worth it.</p>
-				<a href="/sign-in" class="btn btn-primary">Sign up now!</a>
-			</div>
-		</div>
-	</div>
-</div>
+<div>
 
+  <div class="navbar bg-base-200">
+    <a href="#" class="btn btn-ghost normal-case text-xl">TransitHD</a>
+    <div class="navbar-center">Hello, good sky!</div>
+  </div>
+  <div class="h-full v-full bg-gray-300">
+    <div class="hero min-h-screen">
+      <div class="hero-overlay bg-opacity-60"></div>
+      <div class="hero-content text-center text-neutral-content">
+        <div class="max-w-md">
+          <h1 class="mb-5 text-5xl font-bold">Transit HD</h1>
+          <p class="mb-5">It will be pretty expensive but you're worth it.</p>
+          <a href="/sign-in" class="btn btn-primary">Sign up now!</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ src/routes/app/+layout.svelte
@@ -0,0 +1,8 @@
+
+<div>
+  <div class="navbar bg-base-200">
+    <a href="#" class="btn btn-ghost normal-case text-xl">TransitHD</a>
+    <div class="navbar-center">Hello, good sky!</div>
+  </div>
+  <slot />
+</div>
