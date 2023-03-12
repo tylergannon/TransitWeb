b44ccf90632e888ef7686862c00257986ba7d4ab
Tyler Gannon <tgannon@gmail.com>
2023-03-12 08:23:52 -0600

    Move development env back to my macbook

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import fs from 'fs';
+// import fs from 'fs';
 
 
 export default defineConfig({
 	plugins: [sveltekit()],
-  server: {
-    https: {
-      key: fs.readFileSync('/etc/letsencrypt/live/dev.me.transithd.com/privkey.pem'),
-      cert: fs.readFileSync('/etc/letsencrypt/live/dev.me.transithd.com/cert.pem')
-    },
-    host: 'dev.me.transithd.com'
-  },
+  // server: {
+  //   https: {
+  //     key: fs.readFileSync('/etc/letsencrypt/live/dev.me.transithd.com/privkey.pem'),
+  //     cert: fs.readFileSync('/etc/letsencrypt/live/dev.me.transithd.com/cert.pem')
+  //   },
+  //   host: 'dev.me.transithd.com'
+  // },
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
