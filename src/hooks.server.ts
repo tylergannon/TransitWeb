801afe7efd5283da4e9f68696125b7c95b3bb54c
Tyler Gannon <tgannon@gmail.com>
2023-03-14 16:33:41 -0600

    Replace Prisma / NextAuth with Mongoose / Lucia-Auth

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,4 @@
-import type { Provider } from '@auth/core/providers';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import { PrismaClient } from '@prisma/client';
-
-import { SvelteKitAuth } from '@auth/sveltekit';
-import GitHub from '@auth/core/providers/github';
-import Google from '@auth/core/providers/google';
-import EmailProvider from 'next-auth/providers/email';
-
+/*
 import {
 	GOOGLE_OAUTH_ID,
 	GOOGLE_OAUTH_SECRET,
@@ -19,30 +11,4 @@ import {
 	SMTP_FROM,
 	AUTH_SECRET
 } from '$env/static/private';
-
-const prov = (x: any) => x as unknown as Provider;
-
-export const handle = SvelteKitAuth({
-	providers: [
-		prov(GitHub({ clientId: GITHUB_OAUTH_ID, clientSecret: GITHUB_OAUTH_SECRET })),
-		prov(Google({ clientId: GOOGLE_OAUTH_ID, clientSecret: GOOGLE_OAUTH_SECRET })),
-		prov(
-			EmailProvider({
-				server: {
-					host: SMTP_HOST,
-					port: parseInt(SMTP_PORT),
-					auth: {
-						user: SMTP_USER,
-						pass: SMTP_PASS
-					}
-				},
-				from: SMTP_FROM
-			})
-		)
-	],
-	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-	// @ts-ignore
-	adapter: PrismaAdapter(prisma),
-	secret: AUTH_SECRET,
-	callbacks: {}
-});
+*/
