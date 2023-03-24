843d086bcec5554edf0c2a5ed0a36455312df92e
Tyler Gannon <tgannon@gmail.com>
2023-03-23 22:54:33 -0600

    Add component stubs

--- src/routes/app/people/[slug]/+page.svelte
+++ src/routes/app/people/[slug]/+page.svelte
@@ -1,34 +1,41 @@
 <script lang="ts">
+	import { onMount, onDestroy } from 'svelte';
 	import { getContext } from 'svelte';
+	import { derived, type Unsubscriber } from 'svelte/store';
 	import type { Writable } from 'svelte/store';
-	import { utcToZonedTime } from 'date-fns-tz';
+	import { utcToZonedTime, formatInTimeZone } from 'date-fns-tz';
 	import type { PageData } from './$types';
 	import type { PeopleStore } from '$lib/stores/people';
+	import { chartRequest, type HdChartJson } from '$lib/hd/chart';
+	import Designer from '$lib/components/Designer.svelte';
 
 	const userPeople = getContext('userPeople') as Writable<PeopleStore>;
 	export let data: PageData;
-  if (!Object.prototype.hasOwnProperty.call($userPeople, data.person.slug)) {
-    userPeople.update(people => {
-      people[data.person.slug] = data.person
-      return people
-    })
-  }
-  $: person = $userPeople[data.person.slug]
+	if (!Object.prototype.hasOwnProperty.call($userPeople, data.person.slug)) {
+		userPeople.update((people) => {
+			people[data.person.slug] = data.person;
+			return people;
+		});
+	}
+	let chart: HdChartJson | undefined = undefined;
 
+	const person = derived(userPeople, ($userPeople) => $userPeople[data.person.slug]);
+	let unsubscribe: Unsubscriber | undefined = undefined;
+
+	onMount(async () => {
+		unsubscribe = person.subscribe(async (person) => {
+			try {
+				chart = await chartRequest(
+					formatInTimeZone(person.dobUtc, person.tz, 'yyyy-MM-dd'),
+					formatInTimeZone(person.dobUtc, person.tz, 'HH:mm:ss'),
+					person.tz
+				);
+			} catch (e) {
+				console.error('Had an issue with the chart request.', e);
+			}
+		});
+	});
+	onDestroy(()=> unsubscribe && unsubscribe());
 </script>
 
-<div class="p-4">
-	<h1 class="text-4xl font-semibold mb-4">People</h1>
-	<div class="flex flex-col space-y-4">
-		{#each Object.entries($userPeople) as [slug, person]}
-			<div class="flex flex-row space-x-4">
-				<div class="flex flex-col space-y-2">
-					<div class="text-2xl font-semibold">
-						<a href="/app/people/{slug}">{person.firstName} {person.lastName}</a>
-					</div>
-					<div class="text-lg">{utcToZonedTime(person.dobUtc, person.tz).toLocaleString()}</div>
-				</div>
-			</div>
-		{/each}
-	</div>
-</div>
+<Designer />
