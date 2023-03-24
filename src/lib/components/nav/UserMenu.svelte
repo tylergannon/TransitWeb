33d553d67413c13ef378b63ee22a122ee920b910
Tyler Gannon <tgannon@gmail.com>
2023-03-24 03:14:41 -0600

    Move to Skeleton for UI.

---
+++ src/lib/components/nav/UserMenu.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Settings from "carbon-icons-svelte/lib/Settings.svelte";
+</script>
+<div class="card p-4" data-popup="userMenu">
+	<nav class="list-nav">
+		<!-- (optionally you can provde a label here) -->
+		<ul>
+			<li>
+				<a href="/app/settings">
+					<span><Settings /></span>
+					<span class="flex-auto">Settings</span>
+				</a>
+			</li>
+			<!-- ... -->
+		</ul>
+	</nav>
+</div>
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,3 +1,30 @@
+import { redirect } from '@sveltejs/kit';
+import type { LayoutServerLoad } from './$types';
+import { Person, type PersonType } from '$lib/srv/model';
 import { handleServerSession } from '@lucia-auth/sveltekit';
+import type { LayoutServerLoadEvent } from './$types';
 
-export const load = handleServerSession();
+export const load = handleServerSession(async ({ locals }: LayoutServerLoadEvent) => {
+	const userId = (await locals.validate())?.userId || null;
+	if (userId == null) {
+		return { user: null };
+	}
+
+	const { auth, validate } = locals;
+	const { birthplace, dobUtc, firstName, id, lastName, profileImg, tz, tags } = await auth.getUser(
+		userId
+	);
+
+	return {
+		user: {
+			birthplace,
+			dobUtc,
+			firstName,
+			id,
+			lastName,
+			profileImg,
+			tags,
+			tz
+		}
+	};
+});
