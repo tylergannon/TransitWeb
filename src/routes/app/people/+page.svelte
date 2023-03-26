6d8d51b3fa32b9b51b90cdf6943f9ecf82fde9e8
Tyler Gannon <tgannon@gmail.com>
2023-03-25 18:28:37 -0600

    Refactor people store

--- src/routes/app/people/+page.svelte
+++ src/routes/app/people/+page.svelte
@@ -1,23 +1,16 @@
 <script lang="ts">
 	import { getContext } from 'svelte';
-	import type { Writable } from 'svelte/store';
-  import { utcToZonedTime } from 'date-fns-tz';
 	import type { PeopleStore } from '$lib/stores/people';
-  	const userPeople = getContext('userPeople') as Writable<PeopleStore>;
+	import Person from './Person.svelte';
+ 	const userPeople = getContext('userPeople') as PeopleStore;
+
 </script>
 
 <div class="p-4" >
   <h1 class="text-4xl font-semibold mb-4">People</h1>
   <div class="flex flex-col space-y-4">
-    {#each Object.entries($userPeople) as [slug, person]}
-      <div class="flex flex-row space-x-4">
-        <div class="flex flex-col space-y-2">
-          <div class="text-2xl font-semibold">
-            <a href="/app/people/{slug}">{person.firstName} {person.lastName}</a>
-          </div>
-          <div class="text-lg">{utcToZonedTime(person.dobUtc, person.tz).toLocaleString()}</div>
-        </div>
-      </div>
+    {#each Object.values($userPeople) as person}
+      <Person {person} />
     {/each}
   </div>
 </div>
---
+++ src/routes/app/people/Person.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { ClientSidePerson } from "$lib/stores/people";
+	import { utcToZonedTime } from "date-fns-tz";
+  import type { Writable } from "svelte/store";
+  export let person: Writable<ClientSidePerson>;
+</script>
+
+<div class="flex flex-row space-x-4">
+  <div class="flex flex-col space-y-2">
+    <div class="text-2xl font-semibold">
+      <a href="/app/people/{$person.slug}">{$person.firstName} {$person.lastName}</a>
+    </div>
+    <div class="text-lg">{utcToZonedTime($person.dobUtc, $person.tz).toLocaleString()}</div>
+  </div>
+</div>
