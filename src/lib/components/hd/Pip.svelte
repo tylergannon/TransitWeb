fbb7ccb57520e2557971771b186a0af490013aa3
Tyler Gannon <tgannon@gmail.com>
2023-03-28 00:01:25 -0600

    Add SVG for centers and pips
    Remove files

---
+++ src/lib/components/hd/Pip.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let x: number;
+	export let y: number;
+	export let radius: number;
+
+	export let gate: string;
+
+</script>
+
+<circle cx={x} cy={y} r={radius} class="fill-secondary-500" stroke="black" stroke-width="1" />
+<text
+	{x}
+	y={y+1}
+	font-size="{radius*1.3}px"
+	text-anchor="middle"
+	alignment-baseline="middle"
+	fill="black"
+>
+	{gate}
+</text>
