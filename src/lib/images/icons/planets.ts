a2b1ae74ccec6fcaac1f394189923d9090b0070a
Tyler Gannon <tgannon@gmail.com>
2023-04-04 09:49:38 -0600

    SVG files for planet symbols

---
+++ src/lib/images/icons/planets.ts
@@ -0,0 +1,31 @@
+import sun from './sun.svg';
+import chiron from './chiron.svg';
+import earth from './earth.svg';
+import mercury from './mercury.svg';
+import moon from './moon.svg';
+import venus from './venus.svg';
+import mars from './mars.svg';
+import jupiter from './jupiter.svg';
+import saturn from './saturn.svg';
+import uranus from './uranus.svg';
+import neptune from './neptune.svg';
+import pluto from './pluto.svg';
+import northNode from './north-node.svg';
+import southNode from './south-node.svg';
+
+export {
+	sun,
+	chiron,
+	earth,
+	mercury,
+	moon,
+	venus,
+	mars,
+	jupiter,
+	saturn,
+	uranus,
+	neptune,
+	pluto,
+	northNode,
+	southNode
+};
