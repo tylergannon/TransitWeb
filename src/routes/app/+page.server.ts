a0e302d4199c9affba9863abc5934fe2e36fde9f
Tyler Gannon <tgannon@gmail.com>
2023-04-19 20:58:06 -0600

    Delete +page.server.ts

--- src/routes/app/+page.server.ts
+++
@@ -1,9 +0,0 @@
-// import { redirect } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
-
-export const load = (async ({ locals: { auth, validate } }) => {
-	// if (await locals.getSession() !== null) {
-	//   throw redirect(307, "/app")
-	// }
-	return {};
-}) satisfies PageServerLoad;
